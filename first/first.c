f8593aab73b3ceb636326f5dfc7eadcee3e2b5e1
Justin Huh <huh.justin@gmail.com>
2019-05-03 21:02:42 -0400

    working

--- first/first.c
+++ first/first.c
@@ -1,4 +1,15 @@
 #include <stdio.h>
+#include <math.h>
+
+int memRead = 0, memWrite = 0, cacheHit = 0, cacheMiss = 0;
+
+/*
+Cache objects
+Stores Valid Bit, Tag, Cache Block 
+ */
+typedef struct cache{
+    int validBit, tag, cacheBlock;
+} cache;
 
 int main(int argc, char *argv[]){
     int cacheSize = 0;
