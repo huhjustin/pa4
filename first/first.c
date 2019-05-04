ac7fe6c223ac98a5abea737b33c75ab895fd6f12
Justin Huh <huh.justin@gmail.com>
2019-05-03 20:34:48 -0400

    starting

---
+++ first/first.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main(int argc, char *argv[]){
+    int cacheSize = 0, 
+    for(int i=0; argv[i] != NULL; i++){
+        printf("%s", argv[i]);
+        printf("\n");
+    }
+}
