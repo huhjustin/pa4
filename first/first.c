49644f09ef013fe4917c476b9433539fa6cbaaa8
Justin Huh <huh.justin@gmail.com>
2019-05-05 13:28:38 -0400

    working

--- first/first.c
+++ first/first.c
@@ -13,9 +13,6 @@ typedef struct cache{
 } cache;
 
 int main(int argc, char *argv[]){
-    int cacheSize = 0;
-    for(int i=0; argv[i] != NULL; i++){
-        printf("%s", argv[i]);
-        printf("\n");
-    }
+    int cacheSize, assoc, blockSize;
+    
 }
